03c9e26b254a2646ee848e22011ea04fecb7b599
kinglancer <kinglancer0214@protonmaiil.com>
2020-07-06 23:29:50 +0200

    Add initial widget sample source for Image Merge App.

---
+++ refrence/styles/widgetgallery.h
@@ -0,0 +1,81 @@
+#ifndef WIDGETGALLERY_H
+#define WIDGETGALLERY_H
+
+#include <QDialog>
+
+QT_BEGIN_NAMESPACE
+class QCheckBox;
+class QComboBox;
+class QDateTimeEdit;
+class QDial;
+class QGroupBox;
+class QLabel;
+class QLineEdit;
+class QProgressBar;
+class QPushButton;
+class QRadioButton;
+class QScrollBar;
+class QSlider;
+class QSpinBox;
+class QTabWidget;
+class QTableWidget;
+class QTextEdit;
+QT_END_NAMESPACE
+
+//! [0]
+class WidgetGallery : public QDialog
+{
+    Q_OBJECT
+
+public:
+    WidgetGallery(QWidget *parent = 0);
+
+private slots:
+    void changeStyle(const QString &styleName);
+    void changePalette();
+    void advanceProgressBar();
+
+private:
+    void createTopLeftGroupBox();
+    void createTopRightGroupBox();
+    void createBottomLeftTabWidget();
+    void createBottomRightGroupBox();
+    void createProgressBar();
+
+    QPalette originalPalette;
+
+    QLabel *styleLabel;
+    QComboBox *styleComboBox;
+    QCheckBox *useStylePaletteCheckBox;
+    QCheckBox *disableWidgetsCheckBox;
+//! [0]
+
+    QGroupBox *topLeftGroupBox;
+    QRadioButton *radioButton1;
+    QRadioButton *radioButton2;
+    QRadioButton *radioButton3;
+    QCheckBox *checkBox;
+
+    QGroupBox *topRightGroupBox;
+    QPushButton *defaultPushButton;
+    QPushButton *togglePushButton;
+    QPushButton *flatPushButton;
+
+    QTabWidget *bottomLeftTabWidget;
+    QTableWidget *tableWidget;
+    QTextEdit *textEdit;
+
+    QGroupBox *bottomRightGroupBox;
+    QLineEdit *lineEdit;
+    QSpinBox *spinBox;
+    QDateTimeEdit *dateTimeEdit;
+    QSlider *slider;
+    QScrollBar *scrollBar;
+    QDial *dial;
+
+    QProgressBar *progressBar;
+//! [1]
+};
+//! [1]
+
+#endif
